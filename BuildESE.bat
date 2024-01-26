911a93c9d4e0daf095a90f015b3b4ba3f74f9fd7
Nemanja <nemanja.kojic@gmail.com>
2024-01-25 20:11:09 -0800

    Exception handling patch.

--- BuildESE.bat
+++ BuildESE.bat
@@ -3,6 +3,6 @@
 rem NOTE: Make sure the path does not have white spaces
 set "scriptDir=%~dp0"
 
-cmake --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -H%scriptDir% -B%scriptDir%\build -G "Visual Studio 15 2017" -T host=x64 -A x64
+cmake --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -H%scriptDir% -B%scriptDir%\build -G "Visual Studio 16 2019" -T host=x64 -A x64
 
 cmake --build %scriptDir%/build --config Release --target ALL_BUILD -- /maxcpucount:10
